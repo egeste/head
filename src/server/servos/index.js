37173947987c3cf32646eea13186679e6b6427fa
Steve Regester <sregester@synack.com>
2020-11-20 22:05:32 -0800

    s/pin/channel/

--- src/server/servos/index.js
+++ src/server/servos/index.js
@@ -2,30 +2,30 @@ import Servo from './Servo'
 
 // Neck control
 export const NECK_X_SERVO = new Servo({
-  pin: 0,
+  channel: 0,
   position: 0.5,
   pwmRange: [556, 2420]
 })
 
 // export const NECK_Y_SERVO = new Servo({
-//   pin: 1,
+//   channel: 1,
 //   position: 0.5,
 //   pwmRange: [556, 2420]
 // })
 
 // Jaw control
 export const JAW_SERVO = new Servo({
-  pin: 4,
+  channel: 4,
   position: 0.5
 })
 
 // Eye control
 export const EYES_X_SERVO = new Servo({
-  pin: 14,
+  channel: 14,
   position: 0.5
 })
 
 export const EYES_Y_SERVO = new Servo({
-  pin: 15,
+  channel: 15,
   position: 0.5
 })
