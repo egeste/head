12de7c2ac74c0193c2772a19261085c28ac264be
Steve Regester <sregester@synack.com>
2020-11-20 20:15:50 -0800

    Test

--- test.js
+++ test.js
@@ -1,23 +1,27 @@
 import driverPromise from './src/server/pca9685/driver'
 import { scaleLinear } from 'd3-scale'
 
-const positionToPulse =
-
 driverPromise.then(driver => {
 
-  driver.setDutyCycle(0, 0.25)
-  driver.setPulseLength(0, (
-    scaleLinear()
-      .domain([ 0, 1 ])
-      .range([ 556, 2420 ])
-  )(0.5))
+  // driver.setDutyCycle(0, 0.25)
+  // driver.setPulseLength(0, (
+  //   scaleLinear()
+  //     .domain([ 0, 1 ])
+  //     .range([ 556, 2420 ])
+  // )(0.5))
 
   driver.setDutyCycle(1, 0.25)
-  driver.setPulseLength(1, (
-    scaleLinear()
-      .domain([ 0, 1 ])
-      .range([ 10, 400 ])
-  )(0.5))
+
+  let iterableCount = 0
+  const onSetPulse = () => {
+    iterableCount++
+    const pulseWidth = iterableCount * 100
+
+    driver.setPulseLength(1, pulseWidth, 0, () => {
+      console.log('set pulse width', pulseWidth)
+      if (iterableCount < 10) onSetPulse()
+    })
+  }
 
 //   driver.setDutyCycle(0, 0.25)
 //   driver.setPulseLength(0, (
