ed745551a8aacec6eb8c28064e34bd59678c7f21
Steve Regester <sregester@synack.com>
2020-11-22 17:18:09 -0800

    Math.round()

--- src/server/servos/Servo.js
+++ src/server/servos/Servo.js
@@ -63,7 +63,7 @@ export default class Servo extends EventEmitter {
 
     return driverPromise.then(driver => {
       return new Promise((resolve, reject) => {
-        driver.setPulseLength(this.channel, pulseLength, 0, error => {
+        driver.setPulseLength(this.channel, Math.round(pulseLength), 0, error => {
           this.setBlocked(false)
 
           if (error) return reject(error)
